807ba2e9c59db374254045906e677d2f9d5c18a5
Davi Teixeira <daviteixeira.btm@gmail.com>
2024-09-25 23:42:03 -0300

    :test_tube: test: Invalid login and use of intercept and request.

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,7 +1,7 @@
 describe('Aprendendo conceitos Cypress', () => {
 
   it('1 - Acessando a home da página Automation Exercice', () => {
-    cy.visit('https://automationexercise.com/');
+    cy.visit('/');
     cy.contains('Automation');
     cy.get('h1'); // Acessando a tag
     cy.get('h1').contains('Automation');
@@ -11,13 +11,41 @@ describe('Aprendendo conceitos Cypress', () => {
   });
 
   it('2 - Verificando items para compra', () => {
-    cy.visit('https://automationexercise.com/');
+    cy.visit('/');
     cy.get('.features_items');
     cy.get('.features_items').children().first(); // acessa o primeiro elemento filho de um item
     cy.get('.features_items').children().last(); // acessa o último elemento filho de um item
     cy.get('.features_items').children().eq(2); // acessando o elemento específico de uma array pelo index
 
     cy.get('[data-product-id="2"]') // acessa pelo data-id
-  })
+  });
 
+  it('3 - Usuário faz login com username e senha inválidos', () => {
+    cy.visit('/');
+    cy.get('div.shop-menu').contains('Login').click();
+    cy.contains('Login to your account');
+    cy.get('[data-qa="login-email"]').type('teste@email.com');
+    //cy.get('.login-form').find('input[name="email"]'); // da certo
+    cy.get('[data-qa="login-password"]').type('123456');
+    cy.get('[data-qa="login-button"]').contains('Login').click();
+    cy.contains('Your email or password is incorrect!');
+  });
+
+  it('4 - Colocar item no carrinho e continuar comprando', () => {
+    cy.visit('/');
+    cy.get('[data-product-id="2"]').contains('Add to cart').click();
+    cy.get('#cartModal').contains('Added');
+    cy.get('button.close-modal', {timeout: 5000}).click();
+  });
+
+  it('5 - Acessando página de produtos - usando intercept', () => {
+    cy.visit('/');
+    cy.intercept('GET', 'products');
+    cy.get('.navbar-nav').contains('Products').click();
+    //cy.get('a[href^="/products"]').contains('Products').click();
+  });
+
+  it('6 - GET Produtos - usando request', () => {
+    cy.request('GET', 'api/productsList');
+  });
 })
